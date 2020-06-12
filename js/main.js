b0c8d16fcc068610bdf63ef9e9d1cc83537ec449
Jonathon <63322089+jptownley@users.noreply.github.com>
2020-06-12 19:02:26 -0400

    adjusted main.js imports

--- js/main.js
+++ js/main.js
@@ -1,4 +1,4 @@
-import { geoLocator, geoFindMe } from './modules/locations.js';
+import { geoLocator } from './modules/locations.js';
 import { weatherForecast }from './modules/weather.js';
 geoLocator();
 weatherForecast();
