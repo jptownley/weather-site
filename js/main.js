dbae83c9bedc2fab64c8bea4d163bb9a7a11f19b
Jonathon <63322089+jptownley@users.noreply.github.com>
2020-06-12 19:17:31 -0400

    Update main.js

--- js/main.js
+++ js/main.js
@@ -2,3 +2,4 @@ import { geoLocator } from '../weather-sites/js/modules/locations.js';
 import { weatherForecast }from '../weather-sites/js/modules/weather.js';
 geoLocator();
 weatherForecast();
+//poo
