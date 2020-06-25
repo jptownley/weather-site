e61d346f107100fadef0fb4e5776d23b0d580ccc
Jonathon <63322089+jptownley@users.noreply.github.com>
2020-06-25 14:14:41 -0400

    styling changes

--- js/modules/weather.js
+++ js/modules/weather.js
@@ -1,6 +1,6 @@
 "use strict"
 var placeName = "Brooklyn"
-var weatherUrl = `https://api.openweathermap.org/data/2.5/weather?q=${placeName}&appid=d3a65e519935a92e81d73ae7127b3a6c`;
+var weatherUrl = `https://api.openweathermap.org/data/2.5/weather?q={Brooklyn}&appid=d3a65e519935a92e81d73ae7127b3a6c`;
 
 function weatherForecast(){
     $.ajax ({
